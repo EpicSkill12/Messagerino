7c1ca374e222b82039f1603a1e7a6e11d06eb257
Ben <ben.thiemann@icloud.com>
2025-06-09 15:48:39 +0200

    interfaceHandler: expanded zeigeLoginScreen(), added login(), zeigeMainScreen() and expanded run()

--- handlers/interfaceHandler.py
+++ handlers/interfaceHandler.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
 
 AUFLOESUNG:str = "300x200"
+FONT:tuple[str,int] = ("Arial",12)
 
 class Benutzeroberflaeche():
     def __init__(self):
@@ -8,6 +10,43 @@ class Benutzeroberflaeche():
         self.__fenster.title("Massagerino")
         self.__fenster.geometry(AUFLOESUNG)
     
-#öffne...Menu - Methoden
+        self.zeigeLoginScreen()
 
-benutzeroberfläche:Benutzeroberfläche = Benutzeroberfläche()
+#öffne...Menu - Methoden
+    def zeigeLoginScreen(self) -> None:
+        for widget in self.__fenster.winfo_children():
+            widget.destroy()
+        tk.Label(self.__fenster, text = "Nutzername", font = FONT).pack(pady = 10)
+        self.__eingabe_benutzer = tk.Entry(self.__fenster, font = FONT)
+        self.__eingabe_benutzer.pack()
+
+        tk.Button(
+            self.__fenster,
+            text = "Anmelden",
+            font = FONT,
+            command = self.login
+        ).pack(pady=15)
+    
+    def login(self) -> None:
+        benutzername = self.__eingabe_benutzer.get().strip()
+        if not benutzername:
+            messagebox.showwarning(title = "Einagbefehler", message = "Bitte gib einen Benutzernamen ein.") # type: ignore (für pylance)
+            return
+        self.__aktueller_benutzer = benutzername
+
+        self.zeigeMainScreen()
+
+    def zeigeMainScreen(self) -> None:
+        for widget in self.__fenster.winfo_children():
+            widget.destroy()
+        
+        tk.Label(
+            self.__fenster,
+            text = f"Wilommen, {self.__aktueller_benutzer}!",
+            font = FONT
+        ).pack(pady=20)
+
+    def run(self) -> None:
+        self.__fenster.mainloop()
+
+benutzeroberflaeche:Benutzeroberflaeche = Benutzeroberflaeche()
