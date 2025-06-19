c2a6e1b28b4a234aad3e14f553e0ac2146d43d25
Ben <ben.thiemann@icloud.com>
2025-06-19 22:11:40 +0200

    added MINSIZEX and MINSIZEY to constants

--- config/constants.py
+++ config/constants.py
@@ -3,5 +3,7 @@
 AUFLOESUNG:str = "1000x1000"
 FONT:tuple[str,int] = ("Arial",12)
 TITLEFONT:tuple[str, int, str] = ("Arial", 20, "bold")
+MINSIZEX:int = 500
+MINSIZEY:int = 500
 
 DB_PATH = "datenbank.db"
