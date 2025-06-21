3745c3df1315ede31ed81f108143962f4a7081d9
Christian <abel.christian.acc@gmail.com>
2025-06-21 16:21:51 +0200

    updated constants to english

--- config/constants.py
+++ config/constants.py
@@ -1,11 +1,11 @@
 from custom_types.baseTypes import User
 #* Graphics
-AUFLOESUNG: str = "1000x1000"
+RESOLUTION: str = "1000x1000"
 FONT: tuple[str,int] = ("Arial", 12)
 BIG_FONT: tuple[str,int] = ("Arial", 16)
-TITLEFONT: tuple[str, int, str] = ("Arial", 20, "bold")
-MINSIZEX: int = 500
-MINSIZEY: int = 500
+TITLE_FONT: tuple[str, int, str] = ("Arial", 20, "bold")
+MIN_SIZE_X: int = 500
+MIN_SIZE_Y: int = 500
 
 #* Names
 DEV_USER = User(UUID="278f98b8-4ea0-11f0-bfaa-000000000000", username="debugy", displayName="Debugy")
