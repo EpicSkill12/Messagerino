d70b9fd1993ba404a2747d887f72ca4d2dbbde71
Ben <ben.thiemann@icloud.com>
2025-06-28 14:15:18 +0200

    bugfix in .gitnore

--- tests/hashBeispiel.py
+++ tests/hashBeispiel.py
@@ -5,9 +5,11 @@ def hash_string(input_string: str) -> str:
     sha256_hash.update(input_string.encode('utf-8'))
     return sha256_hash.hexdigest()
 
-def check_hash(input_string: str, expected_hash: str = "0d5d680cb2f86f4a4bcbfa7bf6ef65aba1947d2c09d6a56f7548d42a5c369a25") -> bool:
+def check_hash(input_string: str, expected_hash: str = "b1ad66d9501eae4bbef7a24e9ac888fbfea9c43c557645d0a6995a01b5eecd5a") -> bool:
     """Check if the SHA-256 hash of the input string matches the expected hash."""
     return hash_string(input_string) == expected_hash
-print(check_hash("MeinPasswort1"))
+
+
+print(check_hash("HalloWelt!"))
 
 
