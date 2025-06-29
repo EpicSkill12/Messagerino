d9416aaa5e5d6265e4671ed07fdf871c04f9597e
Ben <ben.thiemann@icloud.com>
2025-06-29 20:04:09 +0200

    added Constants THEME, MAX_SIZE_X and _Y ------ added settings_m.png in assets

--- config/constants.py
+++ config/constants.py
@@ -1,4 +1,5 @@
 from os import path
+from typing import Literal
 
 #* Sicherheit
 UUID_MAX_TRIES: int = 3
@@ -8,9 +9,27 @@ RESOLUTION: str = "1500x1000"
 FONT: tuple[str,int] = ("Arial", 12)
 BIG_FONT: tuple[str,int] = ("Arial", 16)
 TITLE_FONT: tuple[str, int, str] = ("Arial", 20, "bold")
-MIN_SIZE_X: int = 1500
-MIN_SIZE_Y: int = 1000
+MIN_SIZE_X: int = 1000
+MIN_SIZE_Y: int = 750
 INTERFACE_COLOR: str = "#f0f0f0"
+MAX_SIZE_X: int = 1920
+MAX_SIZE_Y: int = 1080
+THEMES: dict[str,dict[Literal["background", "foreground", "buttonBG", "buttonFG", "highlight"], str]] = {
+    "light": {
+        "background": "#F4F4F4",
+        "foreground": "#000000",
+        "buttonBG": "#E0E0E0",
+        "buttonFG": "#000000",
+        "highlight": "#E74C3C"
+    },
+    "dark": {
+        "background": "#2C3E50",
+        "foreground": "#ECF0F1",
+        "buttonBG": "#34495E",
+        "buttonFG": "#ECF0F1",
+        "highlight": "#E74C3C"
+    }
+}
 
 #* Ordner
 ASSETS_FOLDER: str = "assets"
